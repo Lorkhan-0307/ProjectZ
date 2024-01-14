59afe53d3297debef9cb7e86d7e62a94a82fe120
sohu0121 <sohu0121@gmail.com>
2024-01-15 03:57:29 +0900

    Bug Fix

--- Source/ProjectZ/Private/Actor/ZProjectile.cpp
+++ Source/ProjectZ/Private/Actor/ZProjectile.cpp
@@ -37,13 +37,18 @@ void AZProjectile::BeginPlay()
 
 void AZProjectile::OnSphereOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	{
+		return;
+	}
+
 	// TO DO : Play Sound
 	// TO DO : Spawn VPX
-	UE_LOG(LogTemp,Warning,TEXT("OtherActor"));
+
 	if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
 	{
 		TargetASC->ApplyGameplayEffectSpecToSelf(*DamageEffectSpecHandle.Data.Get());
 	}
-	
+
 	Destroy();
 }
