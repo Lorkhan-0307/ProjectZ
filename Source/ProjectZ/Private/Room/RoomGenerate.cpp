615f7bfe8798aa048cf28cda9d030c485a4770f0
JK3WN <yimjay2@naver.com>
2024-02-12 16:32:36 +0900

    Creating Room Generator(WIP)

--- Source/ProjectZ/Private/Room/RoomGenerate.cpp
+++ Source/ProjectZ/Private/Room/RoomGenerate.cpp
@@ -2,6 +2,23 @@
 
 
 #include "Room/RoomGenerate.h"
+#include "Components/InstancedStaticMeshComponent.h"
+
+UStaticMesh* CubeMesh;
+
+// Generates rectangle room with floor, wall, door
+void ARoomGenerate::BasicRoom()
+{
+	for(int i=0; i<RoomX; i++)
+	{
+		for(int j=0; j<RoomY; j++)
+		{
+			Floor->AddInstance(FTransform(FVector(i*TileX, j*TileY, 0)));
+		}
+		Wall->AddInstance(FTransform(FVector(i*TileX, 0, 0)));
+		Wall->AddInstance(FTransform(FVector(i*TileX, RoomY*TileY*20, 0)));
+	}
+}
 
 // Sets default values
 ARoomGenerate::ARoomGenerate()
@@ -9,13 +26,30 @@ ARoomGenerate::ARoomGenerate()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultRoot = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DefaultRoot"));
+	Floor = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Floor"));
+	Wall = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Wall"));
+	CubeMesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'")).Object;
+	SetRootComponent(DefaultRoot);
+	Floor->SetupAttachment(GetRootComponent());
+	Wall->SetupAttachment(GetRootComponent());
+	
+	Floor->SetStaticMesh(CubeMesh);
+	Floor->SetWorldScale3D(FVector(2, 2, 0.1));
+	Floor->SetRelativeLocation(FVector(100, 100, 0));
+	Wall->SetStaticMesh(CubeMesh);
+	Wall->SetWorldScale3D(FVector(2, 0.1, 2));
+	Wall->SetRelativeLocation(FVector(100, 0, 100));
+
+	RoomX = 1;
+	RoomY = 1;
 }
 
 // Called when the game starts or when spawned
 void ARoomGenerate::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	BasicRoom();
 }
 
 // Called every frame
--- Source/ProjectZ/Public/Room/RoomGenerate.h
+++ Source/ProjectZ/Public/Room/RoomGenerate.h
@@ -16,15 +16,21 @@ public:
 	ARoomGenerate();
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	// Generates rectangle room with floor, wall, door
+	void BasicRoom();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings") int RoomX;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings") int RoomY;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings") int DoorCount;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings") int TileX = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings") int TileY = 100;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings")
-	int RoomX = 5;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Settings")
-	int RoomY = 5;
+private:
+	UPROPERTY(EditAnywhere, Category = "Component") UStaticMeshComponent* DefaultRoot;
+	UPROPERTY(EditAnywhere, Category = "Component") UInstancedStaticMeshComponent* Floor;
+	UPROPERTY(EditAnywhere, Category = "Component") UInstancedStaticMeshComponent* Wall;
 };
